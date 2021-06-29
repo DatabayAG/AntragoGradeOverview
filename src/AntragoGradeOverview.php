3dbf2541d58c412ff9e114346f308e5d198b7085
Marvin Beym <mBeym@databay.de>
2021-06-29 16:11:39 +0200

    Added overview of grades for the current user

--- src/AntragoGradeOverview.php
+++ src/AntragoGradeOverview.php
@@ -13,19 +13,32 @@ use ilUIPluginRouterGUI;
 use ilAntragoGradeOverviewUIHookGUI;
 use ilAchievementsGUI;
 use ilPersonalDesktopGUI;
-use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 use ilCtrl;
-use Twig_Error_Runtime;
-use Twig_Error_Loader;
-use Twig_Error_Syntax;
 use ILIAS\Plugin\AntragoGradeOverview\Repository\GradeDataRepository;
+use ilObjUser;
+use ILIAS\UI\Factory;
+use ILIAS\UI\Renderer;
+use ILIAS\Plugin\AntragoGradeOverview\Model\GradeData;
+use ilSetting;
 
 class AntragoGradeOverview
 {
     public const AGOP_GRADES_TAB = "agop_grades_tab";
     /**
-     * @var \ilObjUser
+     * @var ilSetting
+     */
+    protected $settings;
+    /**
+     * @var Renderer
+     */
+    protected $renderer;
+    /**
+     * @var Factory
+     */
+    protected $factory;
+    /**
+     * @var ilObjUser
      */
     protected $user;
     /**
@@ -68,10 +81,14 @@ class AntragoGradeOverview
         $this->ctrl = $this->dic->ctrl();
         $this->lng = $dic->language();
         $this->lng->loadLanguageModule("pd");
+        $this->lng->loadLanguageModule("esc");
+
         $this->user = $dic->user();
         $this->mainTpl = $dic->ui()->mainTemplate();
         $this->plugin = ilAntragoGradeOverviewPlugin::getInstance();
-
+        $this->factory = $dic->ui()->factory();
+        $this->renderer = $dic->ui()->renderer();
+        $this->settings = new ilSetting(ilAntragoGradeOverviewPlugin::class);
         $this->gradeDataRepo = GradeDataRepository::getInstance();
     }
 
@@ -92,15 +109,64 @@ class AntragoGradeOverview
             $this->mainTpl->getStandardTemplate();
         }
 
-        $gradesData = $this->gradeDataRepo->readAll($this->user->getId());
 
-        $this->mainTpl->setContent("TEMPORARY");
+        $gradesOverviewHtml = $this->buildGradesOverview($this->gradeDataRepo->readAll($this->user->getId()));
+
+        $this->mainTpl->setContent($gradesOverviewHtml);
 
         if ($this->plugin->isAtLeastIlias6()) {
             $this->dic->ui()->mainTemplate()->printToStdOut();
         } else {
             $this->mainTpl->show();
         }
+    }
+
+    /**
+     * @param GradeData[] $gradesData
+     */
+    protected function buildGradesOverview(array $gradesData) : string
+    {
+        $entries = [];
+        $gradePassedThreshold = (float) $this->settings->get("gradePassedThreshold", 4.5);
+        foreach ($gradesData as $gradeData) {
+            $item = $this->factory->item()->standard($gradeData->getSubjectName())
+                ->withProperties([
+                    $this->plugin->txt("instructor") => $gradeData->getInstructorName(),
+                    $this->lng->txt("date") => $gradeData->getDate()->format("d.m.Y"),
+                    $this->plugin->txt("grade") => number_format($gradeData->getGrade(), 1, ",", "."),
+                    $this->plugin->txt("rating_points") => $gradeData->getEvaluation(),
+                    $this->lng->txt("status") => $this->buildStatus($gradeData->getGrade() < $gradePassedThreshold),
+                ]);
+            $entries[] = $this->factory->item()->group("", [$item]);
+        }
+
+        $list = $this->factory->panel()->listing()->standard(
+            sprintf($this->plugin->txt("gradesOverviewOfUser"), $this->user->getFirstname(), $this->user->getLastname()
+        ), $entries);
+        return $this->renderer->render($list);
+    }
+
+    /**
+     * @param bool $passed
+     * @return string
+     */
+    protected function buildStatus(bool $passed = true) : string
+    {
+        if($passed) {
+            return $this->plugin->txt("passed") . " " . $this->buildImageIcon(ilUtil::getImagePath("icon_ok.svg"), "");
+        } else {
+            return $this->plugin->txt("failed") . " " . $this->buildImageIcon(ilUtil::getImagePath("icon_not_ok.svg"), "");
+        }
+    }
+
+    /**
+     * @param $src
+     * @param $alt
+     * @return string
+     */
+    protected function buildImageIcon($src, $alt)
+    {
+        return "<img border=\"0\" align=\"middle\" src=\"" . $src . "\" alt=\"" . $alt . "\" />";
     }
 
     protected function drawHeader() : void
