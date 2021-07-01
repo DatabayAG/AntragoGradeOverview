3a2f577c9bdbd039675cea90d6f1abc373d6fb87
Marvin Beym <mBeym@databay.de>
2021-07-01 08:40:23 +0200

    Added main menu provider

---
+++ src/Provider/MainMenu.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ILIAS\Plugin\AntragoGradeOverview\Provider;
+
+use ILIAS\GlobalScreen\Scope\MainMenu\Provider\AbstractStaticPluginMainMenuProvider;
+use ilAntragoGradeOverviewPlugin;
+use ilUIPluginRouterGUI;
+use ilAntragoGradeOverviewUIHookGUI;
+
+class MainMenu extends AbstractStaticPluginMainMenuProvider
+{
+    /**
+     * @var ilAntragoGradeOverviewPlugin
+     */
+    protected $plugin;
+
+    /**
+     * Handles the main menu item
+     * @return array
+     */
+    public function getStaticTopItems() : array
+    {
+        $showMainMenuItem = $this->plugin->settings->get("showMainMenuItem", false);
+        $mainMenuItem = $this->mainmenu
+            ->topLinkItem($this->if->identifier("agop_mainmenu_item"))
+            ->withTitle($this->plugin->txt("grades"))
+            ->withAction($this->dic->ctrl()->getLinkTargetByClass([
+                ilUIPluginRouterGUI::class,
+                ilAntragoGradeOverviewUIHookGUI::class
+            ], "showGradesOverview"))
+            ->withVisibilityCallable(
+                function () use ($showMainMenuItem) {
+                    return $showMainMenuItem &&
+                        !$this->dic->user()->isAnonymous();
+                }
+            );
+
+        return [$mainMenuItem];
+    }
+
+    public function getStaticSubItems() : array
+    {
+        return [];
+    }
+}
