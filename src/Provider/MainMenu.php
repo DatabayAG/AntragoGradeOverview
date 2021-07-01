7eb2213dcd1d79491adf48be4d796caff0e3a45c
Marvin Beym <mBeym@databay.de>
2021-07-01 12:37:05 +0200

    Added ilias version check for achievements sub main menu item

--- src/Provider/MainMenu.php
+++ src/Provider/MainMenu.php
@@ -42,8 +42,15 @@ class MainMenu extends AbstractStaticPluginMainMenuProvider
         return [$mainMenuItem];
     }
 
+    /**
+     * Adds a sub item to the achievements main menu entry when ilias version is at least ilias 6
+     * @return array
+     */
     public function getStaticSubItems() : array
     {
+        if(!$this->plugin->isAtLeastIlias6()) {
+           return [];
+        }
         $achievementsGrades = $this->mainmenu
             ->link($this->if->identifier("agop_achievements_grades_subItem"))
             ->withPosition(999)
