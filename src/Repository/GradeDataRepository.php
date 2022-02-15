d4b39a82f2ff8eddb956e1e98e004bfd49b75362
Marvin Beym <mBeym@databay.de>
2022-02-15 09:25:41 +0100

    Update GradeData repository

--- src/Repository/GradeDataRepository.php
+++ src/Repository/GradeDataRepository.php
@@ -6,8 +6,9 @@ namespace ILIAS\Plugin\AntragoGradeOverview\Repository;
 
 use ilDBInterface;
 use ILIAS\Plugin\AntragoGradeOverview\Model\GradeData;
-use DateTime;
 use Exception;
+use ilPDOStatement;
+use DateTime;
 
 class GradeDataRepository
 {
@@ -52,47 +53,55 @@ class GradeDataRepository
     }
 
     /**
-     * Returns all rows from the database table
+     * TODO: Implement or remove/replace
+     * Returns all rows from the database table filtered and sorted
      * @param string $matriculation
      * @param string $dateOrder
      * @param string $subjectOrder
      * @return GradeData[]
      * @throws Exception
      */
-    public function readAll(string $matriculation, string $dateOrder, string $subjectOrder) : array
+    public function readAllByMatriculation(string $matriculation, string $dateOrder, string $subjectOrder) : array
     {
+        /*
         $dateOrder = strtoupper($dateOrder);
         $subjectOrder = strtoupper($subjectOrder);
 
         $result = $this->db->query(
-            "SELECT * FROM " . self::TABLE_NAME . " WHERE matrikel LIKE " . $this->db->quote("%" . $matriculation, "text") . "ORDER BY date {$dateOrder}, subject_name {$subjectOrder}"
+            "SELECT * FROM " . self::TABLE_NAME . " WHERE matrikel LIKE " . $this->db->quote("%" . $matriculation,
+                "text") . "ORDER BY date {$dateOrder}, subject_name {$subjectOrder}"
         );
 
+        return $this->mapResult($result);
+        */
+    }
+
+    /**
+     * Returns all rows from the database table
+     * @return GradeData[]
+     * @throws Exception
+     */
+    public function readAll() : array
+    {
+        $result = $this->db->query(
+            "SELECT * FROM " . self::TABLE_NAME
+        );
+
+        return $this->mapResult($result);
+    }
+
+    /**
+     * @param ilPDOStatement $result
+     * @return GradeData[]
+     * @throws Exception
+     */
+    private function mapResult(ilPDOStatement $result) : array
+    {
         $gradesData = [];
 
         foreach ($this->db->fetchAll($result) as $data) {
             $gradesData[] = (new GradeData())
-                ->setId((int) $data["id"])
-                ->setNoteId((int) $data["note_id"])
-                ->setMatrikel((string) $data["matrikel"])
-                ->setStg((string) $data["stg"])
-                ->setSubjectNumber((string) $data["subject_number"])
-                ->setSubjectShortName((string) $data["subject_short_name"])
-                ->setSubjectName((string) $data["subject_name"])
-                ->setSemester((int) $data["semester"])
-                ->setInstructorName((string) $data["instructor_name"])
-                ->setType((string) $data["type"])
-                ->setDate(new DateTime((string) $data["date"]))
-                ->setGrade((float) $data["grade"])
-                ->setEvaluation((float) $data["evaluation"])
-                ->setAverageEvaluation((float) $data["average_evaluation"])
-                ->setCredits((float) $data["credits"])
-                ->setSeatNumber((int) $data["seat_number"])
-                ->setStatus((string) $data["status"])
-                ->setSubjectAuthorization((bool) $data["subject_authorization"])
-                ->setRemark((string) $data["remark"])
-                ->setCreatedAt(new DateTime((string) $data["created_at"]))
-                ->setModifiedAt(new DateTime((string) $data["modified_at"]));
+                ->setDataByAnnotation($data, "@dbCol");
         }
 
         return $gradesData;
@@ -107,71 +116,110 @@ class GradeDataRepository
     {
         $affected_rows = (int) $this->db->manipulateF(
             "INSERT INTO " . self::TABLE_NAME .
-            " (id, note_id, matrikel, stg, subject_number, subject_short_name, subject_name, semester, instructor_name, type, date, grade, evaluation, average_evaluation, credits, seat_number, status, subject_authorization, remark, created_at, modified_at) VALUES " .
-            "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-            ["integer",
-             "integer",
-             "text",
-             "text",
-             "text",
-             "text",
-             "text",
-             "integer",
-             "text",
-             "text",
-             "date",
-             "float",
-             "float",
-             "float",
-             "float",
-             "integer",
-             "text",
-             "integer",
-             "text",
-             "date",
-             "date"
+            " (id, fp_id_nr, tln_id, tln_name_long, semester, semester_location, date, subject_name, dozent, grade, ects_pkt_tn, passed, error_text, number_of_repeats, created_at, modified_at) VALUES " .
+            "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            [
+                "integer",
+                "integer",
+                "integer",
+                "text",
+                "text",
+                "text",
+                "timestamp",
+                "text",
+                "text",
+                "float",
+                "float",
+                "integer",
+                "text",
+                "integer",
+                "timestamp",
+                "timestamp",
             ],
             [
                 $this->db->nextId(self::TABLE_NAME),
-                $gradeData->getNoteId(),
-                $gradeData->getMatrikel(),
-                $gradeData->getStg(),
-                $gradeData->getSubjectNumber(),
-                $gradeData->getSubjectShortName(),
-                $gradeData->getSubjectName(),
+                $gradeData->getFpIdNr(),
+                $gradeData->getTlnId(),
+                $gradeData->getTlnNameLong(),
                 $gradeData->getSemester(),
-                $gradeData->getInstructorName(),
-                $gradeData->getType(),
-                $gradeData->getDate()->format("Y-m-d"),
+                $gradeData->getSemesterLocation(),
+                $gradeData->getDate() ? $gradeData->getDate()->format("Y-m-d H:i:s") : null,
+                $gradeData->getSubjectName(),
+                $gradeData->getDozent(),
                 $gradeData->getGrade(),
-                $gradeData->getEvaluation(),
-                $gradeData->getAverageEvaluation(),
-                $gradeData->getCredits(),
-                $gradeData->getSeatNumber(),
-                $gradeData->getStatus(),
-                $gradeData->isSubjectAuthorization(),
-                $gradeData->getRemark(),
-                $gradeData->getCreatedAt()->format("Y-m-d"),
-                $gradeData->getModifiedAt()->format("Y-m-d")
+                $gradeData->getEctsPktTn(),
+                $gradeData->isPassed(),
+                $gradeData->getErrorText(),
+                $gradeData->getNumberOfRepeats(),
+                (new DateTime("now"))->format("Y-m-d H:i:s"),
+                (new DateTime("now"))->format("Y-m-d H:i:s")
             ]
         );
         return $affected_rows === 1;
     }
 
+    public function update(GradeData $gradeData) : bool
+    {
+        $affected_rows = (int) $this->db->manipulateF(
+            "UPDATE " . self::TABLE_NAME .
+            " SET fp_id_nr=%s, tln_id=%s, tln_name_long=%s, semester=%s, semester_location=%s, date=%s, subject_name=%s, dozent=%s, grade=%s, ects_pkt_tn=%s, passed=%s, error_text=%s, number_of_repeats=%s, modified_at=%s WHERE id=%s",
+            [
+                "integer",
+                "integer",
+                "text",
+                "text",
+                "text",
+                "timestamp",
+                "text",
+                "text",
+                "float",
+                "float",
+                "integer",
+                "text",
+                "integer",
+                "timestamp",
+                "integer",
+            ],
+            [
+                $gradeData->getFpIdNr(),
+                $gradeData->getTlnId(),
+                $gradeData->getTlnNameLong(),
+                $gradeData->getSemester(),
+                $gradeData->getSemesterLocation(),
+                $gradeData->getDate() ? $gradeData->getDate()->format("Y-m-d H:i:s") : null,
+                $gradeData->getSubjectName(),
+                $gradeData->getDozent(),
+                $gradeData->getGrade(),
+                $gradeData->getEctsPktTn(),
+                $gradeData->isPassed(),
+                $gradeData->getErrorText(),
+                $gradeData->getNumberOfRepeats(),
+                (new DateTime("now"))->format("Y-m-d H:i:s"),
+                $gradeData->getId()
+            ]);
+
+        return $affected_rows === 1;
+    }
+
     /**
      * Creates multiple new rows in the database table
-     * @param GradeData[] $gradesData
+     * @param array{'new': GradeData[], 'changed': GradeData[], 'unchanged': GradeData[]} $mappedDatasets
      */
-    public function import(array $gradesData) : bool
+    public function import(array $mappedDatasets) : bool
     {
-        $this->deleteAll();
+        $newDatasets = $mappedDatasets["new"];
+        $changedDatasets = $mappedDatasets["changed"];
 
         $affectedRows = 0;
-        foreach ($gradesData as $gradeData) {
-            $affectedRows += $this->create($gradeData);
+        foreach ($newDatasets as $newDataset) {
+            $affectedRows += $this->create($newDataset);
         }
 
-        return $affectedRows === count($gradesData);
+        foreach ($changedDatasets as $changedDataset) {
+            $affectedRows += $this->update($changedDataset);
+        }
+
+        return $affectedRows === count($newDatasets) + count($changedDatasets);
     }
 
     /**
